eceb56f351ddf2005e64d869dcfdd7a34074f9f8
Grant <grantlevy7@gmail.com>
2019-06-28 00:29:47 -0400

    first commit

---
+++ Assignment5/src/cs3500/marblesolitaire/model/hw02/HorizontalMove.java
@@ -0,0 +1,36 @@
+package cs3500.marblesolitaire.model.hw02;
+
+/**
+ * This class implements the attributes stored in the IRule interface as a rule for Marble
+ * Solitaire. This rule determines if a horizontal move is valid per movement policy.
+ */
+
+public class HorizontalMove implements IRule {
+
+  /**
+   * Determines if the move to and from a location is valid based on the configuration of a
+   * gameboard.
+   *
+   * @param fromRow the starting row
+   * @param fromCol the starting column
+   * @param toRow the destination row
+   * @param toCol the destination column
+   * @param board the gameboard to be used to determine if the move is valid
+   * @return a boolean showing if the move is considered valid or invalid based on locations and
+   *          board configuration
+   */
+  @Override
+  public boolean validMove(int fromRow, int fromCol, int toRow, int toCol, IBoard board) {
+    if (board.isOutOfBounds(fromRow, fromCol) || board.isOutOfBounds(toRow, toCol)) {
+      return false;
+    }
+    if (fromRow == toRow) {
+      if (Math.abs(fromCol - toCol) == 2) {
+        if (!(board.isEmpty(toRow, (fromCol + toCol) / 2))) {
+          return board.isEmpty(toRow, toCol);
+        }
+      }
+    }
+    return false;
+  }
+}
---
+++ Assignment5/src/cs3500/marblesolitaire/model/hw02/VerticalMove.java
@@ -0,0 +1,36 @@
+package cs3500.marblesolitaire.model.hw02;
+
+/**
+ * This class implements the attributes stored in the IRule interface as a rule for Marble
+ * Solitaire. This rule determines if a vertical move is valid per movement policy.
+ */
+
+public class VerticalMove implements IRule {
+
+  /**
+   * Determines if the move to and from a location is valid based on the configuration of a
+   * gameboard.
+   *
+   * @param fromRow the starting row
+   * @param fromCol the starting column
+   * @param toRow the destination row
+   * @param toCol the destination column
+   * @param board the gameboard to be used to determine if the move is valid
+   * @return a boolean showing if the move is considered valid or invalid based on locations and
+   *          board configuration
+   */
+  @Override
+  public boolean validMove(int fromRow, int fromCol, int toRow, int toCol, IBoard board) {
+    if (board.isOutOfBounds(fromRow, fromCol) || board.isOutOfBounds(toRow, toCol)) {
+      return false;
+    }
+    if (fromCol == toCol) {
+      if (Math.abs(fromRow - toRow) == 2) {
+        if (!(board.isEmpty((fromRow + toRow) / 2, toCol))) {
+          return board.isEmpty(toRow, toCol);
+        }
+      }
+    }
+    return false;
+  }
+}
